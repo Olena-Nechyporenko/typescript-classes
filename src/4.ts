06c47f296d3b07155b94e13c8c11c375277b02a3
Olena-Nechyporenko <127235568+Olena-Nechyporenko@users.noreply.github.com>
2023-11-27 19:12:24 +0200

    Finished all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(this: Key) {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
